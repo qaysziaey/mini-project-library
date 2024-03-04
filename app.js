da6c38f5c088f92ee9807f74088a870060a14891
Qais Ziaey <qziaey@gmail.com>
2024-03-04 11:18:19 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () =>
+  console.log(`Express app listening on port ${PORT}!`)
+);
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
