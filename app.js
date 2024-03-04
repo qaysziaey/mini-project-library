578c43b4b190b34574c064d20a6e72fc7a78b903
Qais Ziaey <qziaey@gmail.com>
2024-03-04 14:34:33 +0100

    minor changes

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Book = require("../model/Book");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8080;
+
+app.get("/", async (req, res) => {
+  await connect();
+  const books = await Book.find().populate("user", "name");
+  if (!books.length) {
+    return res.json({ message: "Notes not found" });
+  }
+
+  res.json(books);
+});
 
 const server = app.listen(PORT, () =>
   console.log(`Express app listening on port ${PORT}!`)
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const Note = require("../model/Note");
+const connect = require("../lib/connectDB");
+const userRoute = require("./users.route");
+
+const route = Router();
+route.use("/:user", userRoute);
+
+route.get("/", async (req, res) => {
+  await connect();
+  const notes = await Note.find().populate("user", "name");
+  if (!notes.length) {
+    return res.json({ message: "Notes not found" });
+  }
+
+  res.json(notes);
+});
+
+route.get("/users", async (req, res) => {
+  await connect();
+  const notes = await Note.find().populate("user", "name");
+
+  if (!notes.length) {
+    return res.json({ message: "Notes not found" });
+  }
+
+  res.json(notes);
+});
+module.exports = route;
