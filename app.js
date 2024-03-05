20e75ada9797adb3a423903ffe82d35573eede5c
Qais Ziaey <qziaey@gmail.com>
2024-03-05 12:29:24 +0100

    Modify books request

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Get all the Books
 app.get("/", getBooks);
